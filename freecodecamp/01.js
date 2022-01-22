17946c4d5d5f9b0f7bcc58c7ceeb239fe4961653
boevoiverolet <bjha78rus@yandex.ru>
2022-01-22 14:45:33 +0300

    CamelCaseOver

--- freecodecamp/01.js
+++ freecodecamp/01.js
@@ -1,2 +1,11 @@
 var myFirstName = "boevoi";
 var myLastName = "vertolet";
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
