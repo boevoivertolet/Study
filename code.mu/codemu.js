96d6fd285c438b5b35adfd9d6d0a0cd36645e0ca
boevoiverolet <bjha78rus@yandex.ru>
2022-02-28 13:05:14 +0300

    ||||

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,8 +1,8 @@
 "use strict"
-let num1 = -10;
-let num2 = -10;
+let num1 = -5;
+let num2 = 15;
 
-if (num1 >=0 || num2 >=0){
+if (num1 >=0 || num2 >= 0){
 	console.log('true');
 }else{
 	console.log('false');
