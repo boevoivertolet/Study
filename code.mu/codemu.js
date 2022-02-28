70c7c2f4592ea2e35c2d81051e18e1b5179da6e4
boevoiverolet <bjha78rus@yandex.ru>
2022-02-28 17:30:19 +0300

    elsif123

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,4 +1,14 @@
 "use strict"
-let test = 1;
-if(test) alert('true');
+let day = 1;
 
+if (day >= 1 && day <=10) {
+	alert('1 dekada');
+}
+
+if (day >= 10 && day < 20) {
+	alert('2 dekada');
+}
+
+if (day >= 20 && day <= 31) {
+	alert('3 dekada');
+}
