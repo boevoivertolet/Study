c03afca9da3886ce3e9a32c203c3309aaeb11943
boevoiverolet <bjha78rus@yandex.ru>
2022-03-01 11:33:06 +0300

    alerterror

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,10 +1,12 @@
 "use strict"
-let day = 22;
+let day = 15;
 
 if (day >= 1 && day <10) {
 	alert('1 dekada');
-} else if (day >= 10 && day <20 ) {
+} else if (day >= 10 && day <20) {
 	alert('2 dekada');
-} else if (day >= 20) {
+} else if (day >= 20 && day<=31){
 	alert('3 dekada');
+} else if (day>1 && day>31){
+	alert('error');
 }
