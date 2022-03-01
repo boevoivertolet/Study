0f686f298a18baefa8aae1c6c83fc758b280ba8c
boevoiverolet <bjha78rus@yandex.ru>
2022-03-01 14:47:05 +0300

    adult

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,18 +1,14 @@
 "use strict"
-let num = 98;
-let sum = ((num-num % 10)/10)+(num % 10);
-	if (num > 10 && num <99)
-	{
-		if (sum <= 9)
-		{
-			console.log('Сумма цифр однозначна');
-		}
-		else
-		{
-			console.log('сумма цифр двузначна');
-		}
-	}
-	else
-	{
-		console.log('вне диапозона');
-	}
+
+let age = 18;
+let adult = true;
+
+
+
+if (age >= 18) {
+	adult = true;
+} else {
+	adult = false;
+}
+
+console.log(adult);
