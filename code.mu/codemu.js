62f646cd4b8d1cdec31164c39b5011b890a27834
boevoiverolet <bjha78rus@yandex.ru>
2022-03-01 14:56:18 +0300

    lettruefalse

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,14 +1,16 @@
 "use strict"
 
 
-let age = 15;
+let age = 24;
 let result =true;
 
 if (age >= 18) {
+	let result;
+
 	if (age <= 23) {
 		result = 'от 18 до 23';
 	} else {
-		 result = 'больше 23';
+		result = 'больше 23';
 	}
 } else {
 	result = 'меньше 18';
