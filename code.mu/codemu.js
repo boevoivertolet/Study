b5e483880a703ac7c0003a7a5ebbeab9fa611ae2
boevoiverolet <bjha78rus@yandex.ru>
2022-03-01 15:41:02 +0300

    ifelsedlinnastroktask2

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,7 +1,10 @@
 "use strict"
 
 
-let str = '12345'
-	if (str.length >= 3){
-		console.log('!');
+let arr = [1,2,3];
+let size = arr.length;
+	if (size == 3 ){
+		console.log(arr[0]+arr[1]+arr[2]);
+	}else{
+		console.log('Массив не 3 элемента');
 	}
