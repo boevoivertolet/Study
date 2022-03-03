a1694e7fd7df0922e99e7719733de3ce20c9ba74
boevoiverolet <bjha78rus@yandex.ru>
2022-03-03 12:01:35 +0300

    Накопление результата в цикле JavaScript Задача 2

--- code.mu/codemu.js
+++ code.mu/codemu.js
@@ -1,14 +1,14 @@
 "use strict"
 /*Накопление результата в цикле JavaScript
-Задача 1
-Найдите произведение целых чисел от 1 до 20.*/
+Задача 2
+Найдите сумму четных чисел от 2 до 100.*/
 
 
 
 
-let mult = 0;
-	for (let i = 1; i <= 20; i++)
+let sum = 0;
+	for (let i = 2; i <= 100; i += 2)
 	{
-		mult = mult + i;
+		sum = sum + i;
 	}
-		console.log(mult);
+		console.log(sum);
