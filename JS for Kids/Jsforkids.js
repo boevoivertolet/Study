a97af99b5e09e75eef9a38ffe31934ba9c2006d5
boevoiverolet <bjha78rus@yandex.ru>
2022-03-10 16:00:47 +0300

    =^.^=

---
+++ JS for Kids/Jsforkids.js
@@ -0,0 +1,8 @@
+'use strict'
+// Рисуем столько котокиов, сколько захотим!
+var drawCats = function (howManyTimes){
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + '=^.^=');
+        }
+}
+drawCats(10); // Вместо 10 тут может быть другое число.
