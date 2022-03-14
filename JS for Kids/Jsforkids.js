fa4e1c927692225ac09ae0f610b867d0c15d7c27
boevoiverolet <bjha78rus@yandex.ru>
2022-03-14 14:52:08 +0300

    #3. Оператор + или join?

--- JS for Kids/Jsforkids.js
+++ JS for Kids/Jsforkids.js
@@ -1,5 +1,5 @@
 'use strict'
-/*#2. Изощренные дразнилки*/
+/*#3. Оператор + или join?*/
 
 
 
@@ -7,10 +7,10 @@
 
 
 
-var randomBodyParts = ["жопа ", "рожа ", "бошка "];
-var randomAdjectives = ["здоровая ", "тупая ", "вонючая "];
-var randomWords = ["куча дерьма ", "клоака ", "бетономешалка ", "хуйня ", "гниль "];
-var randomAnimalBodyParts = ['зад ', 'хобот  ', 'шея  ', 'зубы ', 'мозги '];
+var randomBodyParts = ["жопа", "рожа", "бошка"];
+var randomAdjectives = ["здоровая", "тупая", "вонючая"];
+var randomWords = ["куча дерьма", "клоака", "бетономешалка", "хуйня", "гниль"];
+var randomAnimalBodyParts = ['зад', 'хобот', 'шея', 'зубы', 'мозги'];
 var animals = ['слона', 'крокодила', 'гориллы', 'жирафа', 'курицы']
 // Выбор случайной части тела из массива randomBodyParts:
 var animal = animals[Math.floor(Math.random() * animals.length)];
@@ -19,10 +19,8 @@ var randomAnimalBodyPart = randomAnimalBodyParts[Math.floor(Math.random() * rand
 var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
 // Выбор случайного прилагательного из массива randomAdjectives:
 var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
-// Выбор случайного слова из массива randomWords:
-var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
-// Соединяем случайные строки в предложение:
-var randomInsult = "У тебя " + randomBodyPart + "еще более " + randomAdjective + "чем "+ randomAnimalBodyPart + animal + "!!!";
+
+var randomInsult =['У тебя', randomBodyPart, "еще более", randomAdjective, "чем", randomAnimalBodyPart, animal + "!!!"].join(' ');
 
     console.log(randomInsult);
 
