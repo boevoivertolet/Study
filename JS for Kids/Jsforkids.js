c8c3f0baedc543dc73f4234a47d63e74900c6b11
boevoiverolet <bjha78rus@yandex.ru>
2022-03-14 13:52:07 +0300

    Генератор случайных дразнилок

--- JS for Kids/Jsforkids.js
+++ JS for Kids/Jsforkids.js
@@ -7,15 +7,15 @@
 
 
 
-var randomBodyParts = ["глаз", "нос", "череп"];
-var randomAdjectives = ["вонючая", "унылая", "дурацкая"];
-var randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+var randomBodyParts = ["жопа", "рожа", "мозги"];
+var randomAdjectives = ["здоровая", "тупая", "вонючая"];
+var randomWords = ["куча дерьма", "клоака", "бетономешалка", "хуйня", "гниль"];
 // Выбор случайной части тела из массива randomBodyParts:
 var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
 // Выбор случайного прилагательного из массива randomAdjectives:
 var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
 // Выбор случайного слова из массива randomWords:
-var randomWord = randomWords[Math.floor(Math.random() * 5)];
+var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 // Соединяем случайные строки в предложение:
 var randomInsult = "У тебя " + randomBodyPart + " словно " +randomAdjective + " " + randomWord + "!!!";
 
